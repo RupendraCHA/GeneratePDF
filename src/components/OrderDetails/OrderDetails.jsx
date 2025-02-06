f8e2c36d23a723e87a7e5c56dec6f59c5c6a17c9
RupendraCHA <chandaluri210@gmail.com>
2025-02-06 19:48:03 +0530

    Changes to Pdf File generation

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
 import "./OrderDetails.css"
-import GeneratePdf from '../GeneratePdf/generatePdf';
-
+import jsPDF from "jspdf"
+import "jspdf-autotable"
 const OrderDetails = () => {
+
+    const generatePdf = (orderDetails) => {
+    
+        const doc = new jsPDF()
+    
+        doc.text("Order Details", 14, 10)
+    
+        const tableColumn = ["Order ID", "Product Name", "Quantity", "Price"];
+        const tableRows = []
+    
+        orderDetails.forEach((order) => {
+            const orderData = [
+                order.id,
+                order.productName,
+                order.quantity,
+                `${order.price}`
+            ]
+            tableRows.push(orderData)
+        })
+    
+        doc.autoTable({
+            head: [tableColumn],
+            body: tableRows,
+            startY: 20
+        })
+    
+        doc.save("Order_Details.pdf")
+    
+    }
 
     const orderDetails = [
         { id: 1, productName: "Laptop", quantity: 2, price: 1000 },
@@ -13,7 +42,7 @@ const OrderDetails = () => {
   return (
     <div className='text-center'>
         <h2 className='text-5xl mb-6 font-bold text-white'>Order Details</h2>
-        <button className='text-0.5xl shadow-xl shadow-black-500/50 text-black border-2 border-yellow-700 bg-white font-semibold cursor-pointer px-2 py-1 rounded-sm' onClick={() => GeneratePdf(orderDetails)}>
+        <button className='text-0.5xl shadow-xl shadow-black-500/50 text-black border-2 border-yellow-700 bg-white font-semibold cursor-pointer px-2 py-1 rounded-sm' onClick={() => generatePdf(orderDetails)}>
             Download PDF
         </button>
     </div>
