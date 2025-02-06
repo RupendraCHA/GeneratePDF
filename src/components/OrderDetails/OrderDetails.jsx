9430d727def2e25990e2eb32b1ea483a7de060f2
RupendraCHA <chandaluri210@gmail.com>
2025-02-06 20:08:37 +0530

    Changes to Pdf File generation

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./OrderDetails.css"
 import jsPDF from "jspdf"
 import "jspdf-autotable"
 const OrderDetails = () => {
+
+    const [isPdfDownloaded, setPdfStatus] = useState(false)
 
     const generatePdf = (orderDetails) => {
     
@@ -30,6 +32,7 @@ const OrderDetails = () => {
         })
     
         doc.save("Order_Details.pdf")
+        setPdfStatus(true)
     
     }
 
@@ -45,6 +48,65 @@ const OrderDetails = () => {
         <button className='text-0.5xl shadow-xl shadow-black-500/50 text-black border-2 border-yellow-700 bg-white font-semibold cursor-pointer px-2 py-1 rounded-sm' onClick={() => generatePdf(orderDetails)}>
             Download PDF
         </button>
+        <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-2" defaultChecked />
+            <div className="collapse-title font-semibold">How do I create an account?</div>
+            <div className="collapse-content text-sm">Click the "Sign Up" button in the top right corner and follow the registration process.</div>
+        </div>
+            <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title font-semibold">I forgot my password. What should I do?</div>
+            <div className="collapse-content text-sm">Click on "Forgot Password" on the login page and follow the instructions sent to your email.</div>
+        </div>
+        <div className="collapse collapse-arrow bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-2" />
+            <div className="collapse-title font-semibold">How do I update my profile information?</div>
+            <div className="collapse-content text-sm">Go to "My Account" settings and select "Edit Profile" to make changes.</div>
+        </div>
+        <div className="carousel carousel-center bg-neutral rounded-box max-w-md space-x-4 p-4">
+  <div className="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559703248-dcaaec9fab78.webp"
+      class="rounded-box" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1565098772267-60af42b81ef2.webp"
+      class="rounded-box" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1572635148818-ef6fd45eb394.webp"
+      class="rounded-box" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1494253109108-2e30c049369b.webp"
+      class="rounded-box" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1550258987-190a2d41a8ba.webp"
+      class="rounded-box" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1559181567-c3190ca9959b.webp"
+      class="rounded-box" />
+  </div>
+  <div className="carousel-item">
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1601004890684-d8cbf643f5f2.webp"
+      class="rounded-box" />
+  </div>
+</div>
+{isPdfDownloaded && <div className="toast toast-bottom">
+  <div className="alert alert-info">
+    <span>Pdf Downloaded.
+    </span>
+    <button className='border-1 px-1 border-white-700 bg-white' onClick={() => setPdfStatus(false)}>X</button>
+  </div>
+</div>}
     </div>
   )
 }
