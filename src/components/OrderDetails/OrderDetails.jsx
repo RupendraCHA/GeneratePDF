960609d511cd811046f9210f0e41c76f71c9471b
RupendraCHA <chandaluri210@gmail.com>
2025-02-06 19:32:57 +0530

    Adding daisyUI

--- src/components/OrderDetails/OrderDetails.jsx
+++ src/components/OrderDetails/OrderDetails.jsx
@@ -13,7 +13,7 @@ const OrderDetails = () => {
   return (
     <div className='text-center'>
         <h2 className='text-5xl mb-6 font-bold text-white'>Order Details</h2>
-        <button className='text-0.5xl shadow-xl shadow-black-500/50 border-2 border-yellow-700 bg-white font-semibold cursor-pointer px-2 py-1 rounded-sm' onClick={() => GeneratePdf(orderDetails)}>
+        <button className='text-0.5xl shadow-xl shadow-black-500/50 text-black border-2 border-yellow-700 bg-white font-semibold cursor-pointer px-2 py-1 rounded-sm' onClick={() => GeneratePdf(orderDetails)}>
             Download PDF
         </button>
     </div>
